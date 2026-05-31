08da763aa7b18bd26169b71aff7765602aba614c
Ruben <rubengorodri@gmail.com>
2026-05-31 17:25:03 +0200

    fix path

--- modules/packages.sh
+++ modules/packages.sh
@@ -1,6 +1,11 @@
 
-pkgs_list="../pkgs/base.list"
+pkgs_list="pkgs/base.list"
 
 # needed: avoids reinstall already installed packages
 # noconfirm: avoids -yes
-pacman -Syu --needed --noconfirm - < "$pkgs_list"
+if pacman -Syu --needed --noconfirm - < "$pkgs_list";
+	echo "Package installed succesfully"
+else
+	echo "ERROR: Something happened installing the packages"
+	exit 1
+fi
